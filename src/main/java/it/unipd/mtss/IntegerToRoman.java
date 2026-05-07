fa5d11dc7c79d8e894a298a977ecc71f9ffcd028
subnetMusk <leonardosoligo21@gmail.com>
2026-05-07 23:28:29 +0200

    Implement Roman numeral converter

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -1,7 +1,33 @@
 package it.unipd.mtss;
 
 public class IntegerToRoman {
+    private static final int[] ARABIC_VALUES = {
+        1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
+    };
+
+    private static final String[] ROMAN_SYMBOLS = {
+        "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+    };
+
+    private IntegerToRoman() {
+        // Private constructor: prevent instantiation
+    }
+
     public static String convert(int number) {
-        return "";
+        if (number < 1 || number > 1000) {
+            throw new IllegalArgumentException("Number must be between 1 and 1000");
+        }
+
+        StringBuilder romanNumber = new StringBuilder();
+        int remainingNumber = number;
+
+        for (int i = 0; i < ARABIC_VALUES.length; i++) {
+            while (remainingNumber >= ARABIC_VALUES[i]) {
+                romanNumber.append(ROMAN_SYMBOLS[i]);
+                remainingNumber -= ARABIC_VALUES[i];
+            }
+        }
+
+        return romanNumber.toString();
     }
 }
